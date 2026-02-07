4ac29dcfe24f87e677f183d2fd7265ed164ceb1e
gustavo.fernandez <gustavo.fernandez@mail.udp.cl>
2026-02-07 22:43:11 +0000

    arreglo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.walletavo.app',
   appName: 'Walletavo',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'http://192.168.1.83:5173',
+    cleartext: true
+  }
 };
 
 export default config;
